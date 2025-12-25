13990f019d8277231190deb9b5ac51d7855fae62
Chase Carter <149976513+Theinfection91@users.noreply.github.com>
2025-12-24 19:26:21 -0500

    Okay can now go to ip address on my phone if on the same Wifi so can now try some POST from ESP32 to C# API that will lead to ingestion

--- CarterCam.API/Program.cs
+++ CarterCam.API/Program.cs
@@ -1,31 +1,35 @@
-public partial class Program
+using System.Net;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Get local IPv4
+string ip = "unknown";
+var hostName = Dns.GetHostName();
+var hostEntry = Dns.GetHostEntry(hostName);
+foreach (var addr in hostEntry.AddressList)
 {
-    private static void Main(string[] args)
+    if (addr.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
     {
-        var builder = WebApplication.CreateBuilder(args);
-
-        // Add services to the container.
-
-        builder.Services.AddControllers();
-        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-        builder.Services.AddOpenApi();
-
-        var app = builder.Build();
-
-        app.MapGet("/", () => "CarterCam API is running!");
-
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.MapOpenApi();
-        }
-
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-        app.MapControllers();
-
-        app.Run();
+        ip = addr.ToString();
+        break;
     }
 }
+
+app.MapGet("/", () => $"CarterCam API is running! IP: {ip}");
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run("http://0.0.0.0:5157");
